b3646b29da5eafe311e8853e12bbc283eb8d37aa
Markgabe <mgabriel@id.uff.br>
2019-07-28 14:36:55 -0300

    some syntax changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-var express = require("express");
-var cors = require("cors");
+const express = require("express");
+const cors = require("cors");
 
-var app = express();
+const app = express();
 
 app.use(cors());
 
 app.use("/", (req, res) => {
-    res.json({"Mensagem": "Hello World"});
+    res.json({Mensagem: "Hello World"});
 })
 
 app.listen(3000);
-
-module.exports = app;
