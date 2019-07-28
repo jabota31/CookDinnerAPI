f67fa00ed33c60b35978eef68ac11dccffec30c0
Markgabe <mgabriel@id.uff.br>
2019-07-28 14:40:01 -0300

    morgan added to log the requests

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const morgan = require("morgan");
 
 const app = express();
 
 app.use(cors());
+
+app.use(morgan("tiny"))
 
 app.use("/", (req, res) => {
     res.json({Mensagem: "Hello World"});
